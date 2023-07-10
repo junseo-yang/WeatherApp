736ebf9dfca5efc00405e3a5741da0ace85ce10f
junseo-yang <jsy724724@gmail.com>
2023-07-10 10:14:24 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Junseo Yang on 2023-07-10.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
